40fc4da34eedee961692935c63d9465437b4fbb7
KaiserMulti <pasgrimaudb@gmail.com>
2024-08-19 18:56:23 +0200

    xss

--- js/script.js
+++ js/script.js
@@ -93,3 +93,17 @@ function showAndHideElementsForRoles(){
         }
     })
 }
+
+/*eviter les failles XSS*/
+
+function sanitizeHtml(text){
+    // Créez un élément HTML temporaire de type "div"
+    const tempHtml = document.createElement('div');
+    
+    // Affectez le texte reçu en tant que contenu texte de l'élément "tempHtml"
+    tempHtml.textContent = text;
+    
+    // Utilisez .innerHTML pour récupérer le contenu de "tempHtml"
+    // Cela va "neutraliser" ou "échapper" tout code HTML potentiellement malveillant
+    return tempHtml.innerHTML;
+}
